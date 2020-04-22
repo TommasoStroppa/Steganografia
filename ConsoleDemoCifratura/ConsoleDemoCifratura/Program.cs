530f39b5e79bcd93a11e8207e1073ace6980a8e1
TommasoStroppa <TommasoStroppa@users.noreply.github.com>
2020-04-22 14:52:20 +0200

    Sviluppo ConsoleDemoCifratura

--- ConsoleDemoCifratura/ConsoleDemoCifratura/Program.cs
+++ ConsoleDemoCifratura/ConsoleDemoCifratura/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleDemoCifratura
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Demo cifratura di una stringa qualsiasi tramite password");
+
+            Console.Write("Stringa da cifrare:");
+            string strEncryprion=Console.ReadLine();
+
+            Console.Write("Passord:");
+            string pswd = Console.ReadLine();
+
+            string strEncrypted=CifraturaDLL.Crypto.AESEncryption(strEncryprion, pswd);
+
+            Console.Write("Stringa cifrata:");
+            Console.WriteLine(strEncrypted);
+
+            Console.Write("Password:");
+            string pswdn = Console.ReadLine();
+
+            try
+            {
+                string strDecrypted = CifraturaDLL.Crypto.AESDecryption(strEncrypted, pswdn);
+                Console.Write("Stringa decifrata:");
+                Console.WriteLine(strDecrypted);
+
+                Console.Write("Premi un tasto per continuare...");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("!ERROR!");
+
+                Console.Write("Premi un tasto per continuare...");
+                Console.ReadKey();
+            }
+
+            
         }
     }
 }
