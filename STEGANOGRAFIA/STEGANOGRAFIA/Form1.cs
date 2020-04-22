a4ea24e8d4034aa39cb0149e4ec36fd554d92a6f
AleSalvi <alessandr023salvi@gmail.com>
2020-04-22 15:38:21 +0200

    scrittura
    completamento del tasto scrivi per l'inserimento di testi nelle immagini caricate

--- STEGANOGRAFIA/STEGANOGRAFIA/Form1.cs
+++ STEGANOGRAFIA/STEGANOGRAFIA/Form1.cs
@@ -13,7 +13,7 @@ namespace Steganografia
     public partial class Form1 : Form
     {
         string percorso = string.Empty;
-        
+        Bitmap img;
         public Form1()
         {
             InitializeComponent();
@@ -34,6 +34,75 @@ namespace Steganografia
                 panel1.Visible = true;
                 panel2.Visible = true;
 
+            }
+        }
+
+        private void scrivi_Click(object sender, EventArgs e)
+        {
+            string messaggio = string.Empty;
+            string da_inserire = testo.Text + "\0";
+            foreach (char c in da_inserire)
+            {
+                string ottetto = Convert.ToString((int)c, 2);
+
+                while (ottetto.Length < 8)
+                {
+                    ottetto = "0" + ottetto;
+                }
+                messaggio += ottetto;
+            }
+
+            img = new Bitmap(immagine.Image);
+
+            int larghezza = img.Width;
+            int altezza = img.Height;
+            int ncaratteri = (larghezza * altezza);
+            if (ncaratteri >= messaggio.Length)
+            {
+                for (int y = 0; y < altezza; y++)
+                {
+                    for (int x = 0; x < larghezza; x++)
+                    {
+                        int posizione = larghezza * y + x;
+
+                        if (posizione < messaggio.Length)
+                        {
+                            Color colore = img.GetPixel(x, y);
+                            int n = colore.R;
+                            int[] a = new int[8];
+
+                            for (int i = 0; n > 0; i++)
+                            {
+                                a[i] = n % 2;
+                                n = n / 2;
+                            }
+
+                            string test = string.Empty;
+
+                            for (int i = a.Length - 1; i > 0; i--)
+                            {
+                                test += a[i].ToString();
+                            }
+
+                            test += messaggio[posizione];
+
+                            int R = Convert.ToInt32(test, 2);
+
+                            int A = colore.A;
+                            int G = colore.G;
+                            int B = colore.B;
+                            img.SetPixel(x, y, Color.FromArgb(A, R, G, B));
+                        }
+                    }
+                }
+
+                immagine.Image = img;
+                testo.Text = "";
+                panel3.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("stringa troppo lunga, la massima lunghezza è " + (ncaratteri / 8 - 1).ToString() + " caratteri");
             }
         }
     }
