1774a483fdcc246eef04a24c93abc3d2c463dde2
TommasoStroppa <TommasoStroppa@users.noreply.github.com>
2020-04-22 14:16:14 +0200

    Sviluppo CifraturaDLL

---
+++ CifraturaDLL/CifraturaDLL/Crypto.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+//fonte:https://github.com/Digiex/MCLauncher.NET/blob/master/MCLauncher.net/Crypto.cs
+
+namespace CifraturaDLL
+{
+    public class Crypto
+    {
+        private static byte[] _salt = Encoding.ASCII.GetBytes("jasdh7834y8hfeur73rsharks214");
+
+        public static string AESEncryption(string plainText, string password)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException("plain text");
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            string cipherText = null;
+            Rijndael aesAlg = null;
+
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, _salt);
+
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    cipherText = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+            return cipherText;
+        }
+
+        public static string AESDecryption(string cipherText, string password)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipher text");
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password");
+
+            string plainText = null;
+            Rijndael aesAlg = null;
+
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, _salt);
+
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                aesAlg.IV = key.GetBytes(aesAlg.BlockSize / 8);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] bytes = Convert.FromBase64String(cipherText);
+
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plainText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+            return plainText;
+        }
+    }
+}
